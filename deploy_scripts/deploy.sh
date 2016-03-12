f633acb3da4a0780d30bdc1dffd9afb81fc1f7ea
Steve <stevenlyall@gmail.com>
2016-03-12 00:47:36 -0800

    set up travis

---
+++ deploy_scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    git init
+
+    git remote add deploy "teamcity_user@24.70.42.226:~/remote/teacher"
+    git config user.name "Travis CI"
+    git config user.email "travisCI@travisci.com"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi
